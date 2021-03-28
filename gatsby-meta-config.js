92fc1a1cb0a632cf59397bd9211645071cc58023
Ely Kahn <ely.kahn@gmail.com>
2021-03-28 04:14:18 -0500

    Update gatsby-meta-config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: 'Dev Ed', // Your website title
-  description: `Ed's Blog`, // Your website description
-  author: 'Ed', // Maybe your name
-  siteUrl: 'https://gatsby-starter-apple.netlify.app', // Your website URL
+  title: 'between a rock and a hard place', // Your website title
+  description: `fuckin stuck`, // Your website description
+  author: 'ely', // Maybe your name
+  siteUrl: 'https://between-rock-and-hard-place.netlify.app', // Your website URL
   lang: 'en', // Language
-  utterances: 'sungik-choi/gatsby-starter-apple-comment', // Github repository to store comments
+  utterances: 'elykahn/rock-talk', // Github repository to store comments
   links: {
-    github: 'https://github.com/sungik-choi/gatsby-starter-apple', // Your github repository
+    github: 'https://github.com/elykahn/gatsby-starter-apple', // Your github repository
   },
   icon: 'src/images/icon.png', //  Favicon Path
 };
