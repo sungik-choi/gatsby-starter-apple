cab3dda07e8a62653c2258412ea33d121a3b20cb
Ed Sungik Choi <sungik.dev@gmail.com>
2023-12-09 22:08:24 +0900

    chore(commitlint): override body max length rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ["@commitlint/config-conventional"] }
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-length": [0, "always", Infinity],
+    "body-max-line-length": [0, "always", Infinity],
+  },
+}
